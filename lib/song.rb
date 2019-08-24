cf8634464780f75a60cf41b765b268c69889a658
Chantel Lowe <chantellowe17@gmail.com>
2019-08-24 13:26:11 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@artists
+  @@genres
+  
+  def Song.count
+    @@count
+  end
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    
+    count += 1
+  end
+  
+  
+  
+end
