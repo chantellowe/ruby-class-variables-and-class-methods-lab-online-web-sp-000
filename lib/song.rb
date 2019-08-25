d005f3b8f4a264a41e311372f33d6032a6b15bd0
Chantel Lowe <chantellowe17@gmail.com>
2019-08-25 02:23:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,10 @@ class Song
 
   def initialize(name, artist, genre)
     @@count += 1
+    @name = name
+    @genre = genre
     @@genres << genre
+    @artist = artist
     @@artists << artist
   end
   
