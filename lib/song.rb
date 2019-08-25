399a853820b3ba205c35013421f73097bc2daf77
Chantel Lowe <chantellowe17@gmail.com>
2019-08-25 01:29:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,16 @@
 class Song
   
-  attr_accessor :name
   
   def self.new(name, artist, genre)
   end
   
-
+  def self.name=(name)
+    @name = name
+  end
+  
+  def self.name
+    @name
+  end
   
   
   
