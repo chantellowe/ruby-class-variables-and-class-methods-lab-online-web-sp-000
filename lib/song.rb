73ab30b175a25c8a15e874960b3e3cd8db2dddad
Chantel Lowe <chantellowe17@gmail.com>
2019-08-25 01:54:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,43 +1,12 @@
 class Song
   
-  attr_accessor :name, :artist_name
-  @@all = []
+  attr_accessor :name, :artist, :genre
   
-  def self.all
-    @@all
-  end
+  @@count = 0
 
-  def save
-    self.class.all << self
+  def initialize
+    @@count += 1
   end
-  
-  def self.create
-    song = self.new
-    song.save
-    song
-  end
-    
-  
-  def self.new(name, artist, genre)
-  end
-  
-  def self.new_by_name(name)
-    song = self.new
-    song.name = name
-    song
-  end
-  
-  def self.create_by_name(name)
-    song = self.new
-    song.name = name
-    song.save
-    song
-  end
-  
-  def self.find_by_name(name)
-    
-  end
-  
   
   
 end
